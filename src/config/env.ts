86186028dc3e36ae648ea408de2f4e5e5c2f515c
bryan <bryan53qm@gmail.com>
2026-02-02 16:40:52 -0500

    CHINGA SU MADRE SEGURIDAD

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,8 @@
 const envConfig = {
     environment: import.meta.env.VITE_ENVIRONMENT || 'production',
     // URLs del backend
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3002/api',
-    socketUrl: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3002/',
+    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://data-uploader-backend.railway.internal/api',
+    socketUrl: import.meta.env.VITE_BACKEND_URL || 'https://data-uploader-backend.railway.internal',
 } as const;
 
 export default envConfig;
