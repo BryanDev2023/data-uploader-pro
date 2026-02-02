2d0e54e4c8ec66af2f82d9fd11ac0f2ae9f79878
bryan <bryan53qm@gmail.com>
2026-02-02 16:35:14 -0500

    Update

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ const envConfig = {
     environment: import.meta.env.VITE_ENVIRONMENT || 'production',
     // URLs del backend
     apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3002/api',
-    socketUrl: import.meta.env.VITE_SOCKET_BASE_URL || 'http://localhost:3002/',
+    socketUrl: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3002/',
 } as const;
 
 export default envConfig;
