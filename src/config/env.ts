191bd4b2d999ee4e5199ccd834ba58604aa7172e
bryan <bryan53qm@gmail.com>
2026-02-02 16:59:05 -0500

    Update ENVS, CESAR...

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ const envConfig = {
     environment: import.meta.env.VITE_ENVIRONMENT || 'production',
     // URLs del backend
     apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://data-uploader-backend.railway.internal/api',
-    socketUrl: import.meta.env.VITE_BACKEND_URL || 'https://data-uploader-backend.railway.internal',
+    socketUrl: import.meta.env.VITE_BACKEND_URL || 'https://data-uploader-backend.railway.internal/api/v1',
 } as const;
 
 export default envConfig;
